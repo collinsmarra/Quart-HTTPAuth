5e47c5cd7e8849441f866e6c30563abf1e36c879
collinsmarra <collinsk498@gmail.com>
2024-01-11 17:57:12 +0300

    Full Async Support
    This commit introduces comprehensive async support to the project as we transition from Flask to Quart. Key improvements include:
    
    - Migrated the project to Quart to take advantage of its full asynchronous capabilities.
    - Ensured full test coverage to maintain code quality and reliability.

--- src/quart_httpauth.py
+++ src/quart_httpauth.py
@@ -35,10 +35,11 @@ class HTTPAuth(object):
         self.error_handler(default_auth_error)
 
     def is_compatible_auth(self, headers):
-        if self.header is None or self.header == 'Authorization':
+        if self.header is None or self.header == "Authorization":
             try:
-                scheme, _ = request.headers.get('Authorization', '').split(
-                    None, 1)
+                scheme, _ = request.headers.get("Authorization", "").split(
+                    None, 1
+                )
             except ValueError:
                 # malformed Authorization header
                 return False
@@ -56,17 +57,18 @@ class HTTPAuth(object):
 
     def error_handler(self, f):
         @wraps(f)
-        def decorated(*args, **kwargs):
-            res = self.ensure_sync(f)(*args, **kwargs)
+        async def decorated(*args, **kwargs):
+            res = await self.ensure_async(f)(*args, **kwargs)
             check_status_code = not isinstance(res, (tuple, Response))
-            res = make_response(res)
+            res = await make_response(res)
             if check_status_code and res.status_code == 200:
-                # if user didn't set status code, use 401
+                # if the user didn't set the status code, use 401
                 res.status_code = 401
-            if 'WWW-Authenticate' not in res.headers.keys():
-                res.headers['WWW-Authenticate'] = self.authenticate_header()
+            if "WWW-Authenticate" not in res.headers.keys():
+                res.headers["WWW-Authenticate"] = self.authenticate_header()
             return res
-        self.auth_error_callback = decorated
+
+        self.auth_error_callback = decorated  # Assign the decorated function
         return decorated
 
     def authenticate_header(self):
@@ -74,16 +76,18 @@ class HTTPAuth(object):
 
     def get_auth(self):
         auth = None
-        if self.header is None or self.header == 'Authorization':
+        if self.header is None or self.header == "Authorization":
             auth = request.authorization
-            if auth is None and \
-                    'Authorization' in request.headers:  # pragma: no cover
+            if (
+                auth is None and "Authorization" in request.headers
+            ):  # pragma: no cover
                 # Flask/Werkzeug versions before 2.3 do not recognize any
                 # authentication types other than Basic or Digest, so here we
                 # parse the header by hand
                 try:
-                    auth_type, token = request.headers['Authorization'].split(
-                        None, 1)
+                    auth_type, token = request.headers["Authorization"].split(
+                        None, 1
+                    )
                     auth = Authorization(auth_type)
                     auth.token = token
                 except (ValueError, KeyError):
@@ -103,16 +107,18 @@ class HTTPAuth(object):
 
         return auth
 
-    def get_auth_password(self, auth):
+    async def get_auth_password(self, auth):
         password = None
 
         if auth and auth.username:
-            password = self.ensure_sync(self.get_password_callback)(
-                auth.username)
+            # password = self.ensure_sync(self.get_password_callback)(
+            password = await self.ensure_async(self.get_password_callback)(
+                auth.username
+            )
 
         return password
 
-    def authorize(self, role, user, auth):
+    async def authorize(self, role, user, auth):
         if role is None:
             return True
         if isinstance(role, (list, tuple)):
@@ -122,8 +128,11 @@ class HTTPAuth(object):
         if user is True:
             user = auth
         if self.get_user_roles_callback is None:  # pragma: no cover
-            raise ValueError('get_user_roles callback is not defined')
-        user_roles = self.ensure_sync(self.get_user_roles_callback)(user)
+            raise ValueError("get_user_roles callback is not defined")
+        #: user_roles = self.ensure_sync(self.get_user_roles_callback)(user)
+        user_roles = await self.ensure_async(self.get_user_roles_callback)(
+            user
+        )
         if user_roles is None:
             user_roles = {}
         elif not isinstance(user_roles, (list, tuple)):
@@ -139,14 +148,16 @@ class HTTPAuth(object):
                 return True
 
     def login_required(self, f=None, role=None, optional=None):
-        if f is not None and \
-                (role is not None or optional is not None):  # pragma: no cover
+        if f is not None and (
+            role is not None or optional is not None
+        ):  # pragma: no cover
             raise ValueError(
-                'role and optional are the only supported arguments')
+                "role and optional are the only supported arguments"
+            )
 
         def login_required_internal(f):
             @wraps(f)
-            def decorated(*args, **kwargs):
+            async def decorated(*args, **kwargs):
                 auth = self.get_auth()
 
                 # Flask normally handles OPTIONS requests on its own, but in
@@ -154,24 +165,30 @@ class HTTPAuth(object):
                 # application, we need to ignore authentication headers and
                 # let the request through to avoid unwanted interactions with
                 # CORS.
-                if request.method != 'OPTIONS':  # pragma: no cover
-                    password = self.get_auth_password(auth)
+                if request.method != "OPTIONS":  # pragma: no cover
+                    password = await self.get_auth_password(auth)
 
                     status = None
-                    user = self.authenticate(auth, password)
+                    user = await self.authenticate(auth, password)
                     if user in (False, None):
                         status = 401
-                    elif not self.authorize(role, user, auth):
+                    elif not await self.authorize(role, user, auth):
                         status = 403
                     if not optional and status:
                         try:
-                            return self.auth_error_callback(status)
+                            return await self.auth_error_callback(status)
                         except TypeError:
-                            return self.auth_error_callback()
+                            return await self.auth_error_callback()
 
-                    g.flask_httpauth_user = user if user is not True \
-                        else auth.username if auth else None
-                return self.ensure_sync(f)(*args, **kwargs)
+                    g.flask_httpauth_user = (
+                        user
+                        if user is not True
+                        else auth.username
+                        if auth
+                        else None
+                    )
+                return await self.ensure_async(f)(*args, **kwargs)
+
             return decorated
 
         if f:
@@ -185,19 +202,20 @@ class HTTPAuth(object):
         return auth.username
 
     def current_user(self):
-        if hasattr(g, 'flask_httpauth_user'):
+        if hasattr(g, "flask_httpauth_user"):
             return g.flask_httpauth_user
 
-    def ensure_sync(self, f):
+    def ensure_async(self, f):
         try:
-            return current_app.ensure_sync(f)
+            #: return current_app.ensure_sync(f)
+            return current_app.ensure_async(f)
         except AttributeError:  # pragma: no cover
             return f
 
 
 class HTTPBasicAuth(HTTPAuth):
     def __init__(self, scheme=None, realm=None):
-        super(HTTPBasicAuth, self).__init__(scheme or 'Basic', realm)
+        super(HTTPBasicAuth, self).__init__(scheme or "Basic", realm)
 
         self.hash_password_callback = None
         self.verify_password_callback = None
@@ -213,28 +231,30 @@ class HTTPBasicAuth(HTTPAuth):
     def get_auth(self):
         # this version of the Authorization header parser is more flexible
         # than Werkzeug's, as it also accepts other schemes besides "Basic"
-        header = self.header or 'Authorization'
+        header = self.header or "Authorization"
         if header not in request.headers:
             return None
-        value = request.headers[header].encode('utf-8')
+        value = request.headers[header].encode("utf-8")
         try:
-            scheme, credentials = value.split(b' ', 1)
-            encoded_username, encoded_password = b64decode(
-                credentials).split(b':', 1)
+            scheme, credentials = value.split(b" ", 1)
+            encoded_username, encoded_password = b64decode(credentials).split(
+                b":", 1
+            )
         except (ValueError, TypeError):
             return None
         try:
-            username = encoded_username.decode('utf-8')
-            password = encoded_password.decode('utf-8')
+            username = encoded_username.decode("utf-8")
+            password = encoded_password.decode("utf-8")
         except UnicodeDecodeError:
             # try to decode again with latin-1, which should always work
-            username = encoded_username.decode('latin1')
-            password = encoded_password.decode('latin1')
+            username = encoded_username.decode("latin1")
+            password = encoded_password.decode("latin1")
 
         return Authorization(
-            scheme, {'username': username, 'password': password})
+            scheme, {"username": username, "password": password}
+        )
 
-    def authenticate(self, auth, stored_password):
+    async def authenticate(self, auth, stored_password):
         if auth:
             username = auth.username
             client_password = auth.password
@@ -242,37 +262,53 @@ class HTTPBasicAuth(HTTPAuth):
             username = ""
             client_password = ""
         if self.verify_password_callback:
-            return self.ensure_sync(self.verify_password_callback)(
-                username, client_password)
+            #: return self.ensure_sync(self.verify_password_callback)(
+            return await self.ensure_async(self.verify_password_callback)(
+                username, client_password
+            )
         if not auth:
             return
         if self.hash_password_callback:
             try:
-                client_password = self.ensure_sync(
-                    self.hash_password_callback)(client_password)
+                # client_password = self.ensure_sync(
+                client_password = await self.ensure_async(
+                    self.hash_password_callback
+                )(client_password)
             except TypeError:
-                client_password = self.ensure_sync(
-                    self.hash_password_callback)(username, client_password)
-        return auth.username if client_password is not None and \
-            stored_password is not None and \
-            hmac.compare_digest(client_password, stored_password) else None
+                # client_password = self.ensure_sync(
+                client_password = await self.ensure_async(
+                    self.hash_password_callback
+                )(username, client_password)
+        return (
+            auth.username
+            if client_password is not None
+            and stored_password is not None
+            and hmac.compare_digest(client_password, stored_password)
+            else None
+        )
 
 
 class HTTPDigestAuth(HTTPAuth):
-    def __init__(self, scheme=None, realm=None, use_ha1_pw=False, qop='auth',
-                 algorithm='MD5'):
-        super(HTTPDigestAuth, self).__init__(scheme or 'Digest', realm)
+    def __init__(
+        self,
+        scheme=None,
+        realm=None,
+        use_ha1_pw=False,
+        qop="auth",
+        algorithm="MD5",
+    ):
+        super(HTTPDigestAuth, self).__init__(scheme or "Digest", realm)
         self.use_ha1_pw = use_ha1_pw
         if isinstance(qop, str):
-            self.qop = [v.strip() for v in qop.split(',')]
+            self.qop = [v.strip() for v in qop.split(",")]
         else:
             self.qop = qop
-        if algorithm.lower() == 'md5':
-            self.algorithm = 'MD5'
-        elif algorithm.lower() == 'md5-sess':
-            self.algorithm = 'MD5-Sess'
+        if algorithm.lower() == "md5":
+            self.algorithm = "MD5"
+        elif algorithm.lower() == "md5-sess":
+            self.algorithm = "MD5-Sess"
         else:
-            raise ValueError(f'Algorithm {algorithm} is not supported')
+            raise ValueError(f"Algorithm {algorithm} is not supported")
         self.random = SystemRandom()
         try:
             self.random.random()
@@ -285,7 +321,7 @@ class HTTPDigestAuth(HTTPAuth):
         self.verify_opaque_callback = None
 
         def _generate_random():
-            return md5(str(self.random.random()).encode('utf-8')).hexdigest()
+            return md5(str(self.random.random()).encode("utf-8")).hexdigest()
 
         def default_generate_nonce():
             session["auth_nonce"] = _generate_random()
@@ -336,54 +372,79 @@ class HTTPDigestAuth(HTTPAuth):
 
     def generate_ha1(self, username, password):
         a1 = username + ":" + self.realm + ":" + password
-        a1 = a1.encode('utf-8')
+        a1 = a1.encode("utf-8")
         return md5(a1).hexdigest()
 
     def authenticate_header(self):
         nonce = self.get_nonce()
         opaque = self.get_opaque()
         if self.qop:
-            return ('{0} realm="{1}",nonce="{2}",opaque="{3}",algorithm="{4}"'
-                    ',qop="{5}"').format(
-                self.scheme, self.realm, nonce,
-                opaque, self.algorithm, ','.join(self.qop))
+            return (
+                '{0} realm="{1}",nonce="{2}",opaque="{3}",algorithm="{4}"'
+                ',qop="{5}"'
+            ).format(
+                self.scheme,
+                self.realm,
+                nonce,
+                opaque,
+                self.algorithm,
+                ",".join(self.qop),
+            )
         else:
             return '{0} realm="{1}",nonce="{2}",opaque="{3}"'.format(
-                self.scheme, self.realm, nonce,
-                opaque)
+                self.scheme, self.realm, nonce, opaque
+            )
 
-    def authenticate(self, auth, stored_password_or_ha1):
-        if not auth or not auth.username or not auth.realm or not auth.uri \
-                or not auth.nonce or not auth.response \
-                or not stored_password_or_ha1:
+    async def authenticate(self, auth, stored_password_or_ha1):
+        if (
+            not auth
+            or not auth.username
+            or not auth.realm
+            or not auth.uri
+            or not auth.nonce
+            or not auth.response
+            or not stored_password_or_ha1
+        ):
             return False
-        if not self.verify_nonce_callback(auth.nonce) or \
-                not self.verify_opaque_callback(auth.opaque):
+        if not self.verify_nonce_callback(
+            auth.nonce
+        ) or not self.verify_opaque_callback(auth.opaque):
             return False
         if auth.qop and auth.qop not in self.qop:  # pragma: no cover
             return False
         if self.use_ha1_pw:
             ha1 = stored_password_or_ha1
         else:
-            a1 = auth.username + ":" + auth.realm + ":" + \
-                stored_password_or_ha1
-            ha1 = md5(a1.encode('utf-8')).hexdigest()
-        if self.algorithm == 'MD5-Sess':
-            ha1 = md5((ha1 + ':' + auth.nonce + ':' + auth.cnonce).encode(
-                'utf-8')).hexdigest()
+            a1 = (
+                auth.username + ":" + auth.realm + ":" + stored_password_or_ha1
+            )
+            ha1 = md5(a1.encode("utf-8")).hexdigest()
+        if self.algorithm == "MD5-Sess":
+            ha1 = md5(
+                (ha1 + ":" + auth.nonce + ":" + auth.cnonce).encode("utf-8")
+            ).hexdigest()
         a2 = request.method + ":" + auth.uri
-        ha2 = md5(a2.encode('utf-8')).hexdigest()
-        if auth.qop == 'auth':
-            a3 = ha1 + ":" + auth.nonce + ":" + auth.nc + ":" + \
-                auth.cnonce + ":auth:" + ha2
+        ha2 = md5(a2.encode("utf-8")).hexdigest()
+        if auth.qop == "auth":
+            a3 = (
+                ha1
+                + ":"
+                + auth.nonce
+                + ":"
+                + auth.nc
+                + ":"
+                + auth.cnonce
+                + ":auth:"
+                + ha2
+            )
         else:
             a3 = ha1 + ":" + auth.nonce + ":" + ha2
-        response = md5(a3.encode('utf-8')).hexdigest()
+        response = md5(a3.encode("utf-8")).hexdigest()
         return hmac.compare_digest(response, auth.response)
 
 
 class HTTPTokenAuth(HTTPAuth):
-    def __init__(self, scheme='Bearer', realm=None, header=None):
+    def __init__(self, scheme="Bearer", realm=None, header=None):
         super(HTTPTokenAuth, self).__init__(scheme, realm, header)
 
         self.verify_token_callback = None
@@ -392,10 +453,11 @@ class HTTPTokenAuth(HTTPAuth):
         self.verify_token_callback = f
         return f
 
-    def authenticate(self, auth, stored_password):
-        token = getattr(auth, 'token', '')
+    async def authenticate(self, auth, stored_password):
+        token = getattr(auth, "token", "")
         if self.verify_token_callback:
-            return self.ensure_sync(self.verify_token_callback)(token)
+            # return self.ensure_sync(self.verify_token_callback)(token)
+            return await self.ensure_async(self.verify_token_callback)(token)
 
 
 class MultiAuth(object):
@@ -404,22 +466,26 @@ class MultiAuth(object):
         self.additional_auth = args
 
     def login_required(self, f=None, role=None, optional=None):
-        if f is not None and \
-                (role is not None or optional is not None):  # pragma: no cover
+        if f is not None and (
+            role is not None or optional is not None
+        ):  # pragma: no cover
             raise ValueError(
-                'role and optional are the only supported arguments')
+                "role and optional are the only supported arguments"
+            )
 
         def login_required_internal(f):
             @wraps(f)
-            def decorated(*args, **kwargs):
+            async def decorated(*args, **kwargs):
                 selected_auth = self.main_auth
                 if not self.main_auth.is_compatible_auth(request.headers):
                     for auth in self.additional_auth:
                         if auth.is_compatible_auth(request.headers):
                             selected_auth = auth
                             break
-                return selected_auth.login_required(
-                    role=role, optional=optional)(f)(*args, **kwargs)
+                return await selected_auth.login_required(
+                    role=role, optional=optional
+                )(f)(*args, **kwargs)
+
             return decorated
 
         if f:
@@ -427,5 +493,5 @@ class MultiAuth(object):
         return login_required_internal
 
     def current_user(self):
-        if hasattr(g, 'flask_httpauth_user'):  # pragma: no cover
+        if hasattr(g, "flask_httpauth_user"):  # pragma: no cover
             return g.flask_httpauth_user
